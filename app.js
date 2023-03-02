1709791176ce61a10fc7c4554a7466b979a233ee
Kai Eugene <kai.yougene@gmail.com>
2023-03-02 12:15:58 -0800

    working display without form info

---
+++ app.js
@@ -0,0 +1,61 @@
+const form = document.querySelector('#popUpForm');
+const overlay = document.querySelector('.overlay');
+const addABookButton = document.querySelector('#popupBtn');
+const title = document.querySelector('#title');
+const author = document.querySelector('#author');
+const numberOfPages = document.querySelector('#pages');
+const readStatus = document.querySelector('#read-status')
+const submitButton = document.querySelector('#submitButton');
+const mainContent = document.querySelector('.main-content');
+let myLibrary = [];
+
+function Book(title, author, pages, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageLength = pages;
+    this.readStatus = readStatus;
+}
+
+form.addEventListener ('submit', (e)=> {
+    e.preventDefault();
+})
+
+submitButton.addEventListener('click', () => {
+    myLibrary.push(new Book(title.value, author.value, numberOfPages.value, readStatus.value));
+    updateDisplay();
+    updateForm();
+    form.style.display = 'none';
+    overlay.style.display = 'none';
+})
+
+addABookButton.addEventListener('click', ()=>{
+    overlay.style.display = 'block';
+    form.style.display = 'block';
+})
+
+
+
+function addBookToLibrary () {
+    
+}
+
+function removeBookFromLibrary () {
+
+}
+
+function updateForm () {
+    title.value = '';
+    author.value = '';
+    numberOfPages.value = '';
+    readStatus.value = '';
+}
+
+function updateDisplay () {
+    myLibrary.forEach((book)=> {
+        if (myLibrary.indexOf(book) == (myLibrary.length - 1)){
+            let card = document.createElement('div');
+            card.setAttribute('class', 'book-card'); 
+            mainContent.appendChild(card);
+        } 
+    })
+}
